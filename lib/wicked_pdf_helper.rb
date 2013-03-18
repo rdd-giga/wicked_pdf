8a54bc3d4d832e93623bb11c1a2b325a614a46c6
LI Daobing <lidaobing@gmail.com>
2013-03-18 15:02:18 +0800

    fix paperclip url

--- lib/wicked_pdf_helper.rb
+++ lib/wicked_pdf_helper.rb
@@ -57,7 +57,8 @@ module WickedPdfHelper
     private
 
     def asset_pathname(source)
-      return File.join(Rails.public_path, source) if source.starts_with?('/') && !source.starts_with?('//')
+      return "http:" + source if source.starts_with?('//')
+      return File.join(Rails.public_path, source) if source.starts_with?('/')
       if Rails.configuration.assets.compile == false
         if ActionController::Base.asset_host
           # asset_path returns an absolute URL using asset_host if asset_host is set
